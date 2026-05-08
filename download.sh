03919c1691449c11413e89d61906f1537efae695
Ferries01 <83286237+Ferries01@users.noreply.github.com>
2026-05-08 12:34:54 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -6,11 +6,12 @@ python3 main.py --url https://www.ipc-services.org/sdms/web/records/ath/excel/ty
 python3 main.py --url https://www.ipc-services.org/sdms/web/records/ath/excel/type/asr --output records/asia.csv --code PAR
 python3 main.py --url https://www.ipc-services.org/sdms/web/records/ath/excel/type/eur --output records/europe.csv --code PAR
 python3 main.py --url https://www.ipc-services.org/sdms/web/records/ath/excel/type/ocr --output records/oceania.csv --code PAR
+python3 main.py --url https://www.ipc-services.org/sdms/web/records/ath/excel/type/pr --output records/paralympics.csv --code PR
+
 
 python3 main.py --url https://www.ipc-services.org/sdms/web/rankings/ath/excel/type/world/list/1155/location/outdoor --output leads/world.csv --code WL
 python3 main.py --url https://www.ipc-services.org/sdms/web/rankings/ath/excel/type/afr/list/1155/location/outdoor --output leads/africa.csv --code PAL
-python3 main.py --url hhttps://www.ipc-services.org/sdms/web/rankings/ath/excel/type/amr/list/1155/location/outdoor --output leads/america.csv --code PAL
+python3 main.py --url https://www.ipc-services.org/sdms/web/rankings/ath/excel/type/amr/list/1155/location/outdoor --output leads/america.csv --code PAL
 python3 main.py --url https://www.ipc-services.org/sdms/web/rankings/ath/excel/type/asr/list/1155/location/outdoor --output leads/asia.csv --code PAL
 python3 main.py --url https://www.ipc-services.org/sdms/web/rankings/ath/excel/type/eur/list/1155/location/outdoor --output leads/europe.csv --code PAL
 python3 main.py --url https://www.ipc-services.org/sdms/web/rankings/ath/excel/type/ocr/list/1155/location/outdoor --output leads/oceania.csv --code PAL
-python3 main.py --url https://www.ipc-services.org/sdms/web/records/ath/excel/type/PR --output records/paralympics.csv --code PR
